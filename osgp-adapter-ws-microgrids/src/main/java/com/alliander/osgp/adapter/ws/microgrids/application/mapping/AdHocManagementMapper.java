48aa0c493697271c69afdeb6cf18e24081dc14c0
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2016-06-09 09:11:55 +0200

    ZS-14 ~ Adds ws-domain mapping

--- osgp-adapter-ws-microgrids/src/main/java/com/alliander/osgp/adapter/ws/microgrids/application/mapping/AdHocManagementMapper.java
+++ osgp-adapter-ws-microgrids/src/main/java/com/alliander/osgp/adapter/ws/microgrids/application/mapping/AdHocManagementMapper.java
@@ -1,5 +1,37 @@
 package com.alliander.osgp.adapter.ws.microgrids.application.mapping;
 
-public class AdHocManagementMapper {
+import org.springframework.stereotype.Component;
 
+import com.alliander.osgp.domain.microgrids.DataRequest;
+import com.alliander.osgp.domain.microgrids.DataResponse;
+import com.alliander.osgp.domain.microgrids.MeasurementResultSystemIdentifier;
+import com.alliander.osgp.domain.microgrids.SystemFilter;
+import com.alliander.osgp.shared.mappers.XMLGregorianCalendarToDateTimeConverter;
+
+import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.impl.ConfigurableMapper;
+
+@Component(value = "microgridsAdhocManagementMapper")
+public class AdHocManagementMapper extends ConfigurableMapper {
+    @Override
+    public void configure(final MapperFactory mapperFactory) {
+
+        mapperFactory.classMap(com.alliander.osgp.adapter.ws.schema.microgrids.adhocmanagement.SystemFilter.class,
+                SystemFilter.class).field("measurementFilter", "measurementFilters").byDefault().register();
+
+        mapperFactory.classMap(com.alliander.osgp.adapter.ws.schema.microgrids.adhocmanagement.GetDataRequest.class,
+                DataRequest.class).field("systemFilter", "systemFilters").byDefault().register();
+
+        mapperFactory
+                .classMap(MeasurementResultSystemIdentifier.class,
+                        com.alliander.osgp.adapter.ws.schema.microgrids.adhocmanagement.MeasurementResultSystemIdentifier.class)
+                .field("measurements", "measdurement").byDefault().register();
+
+        mapperFactory
+                .classMap(DataResponse.class,
+                        com.alliander.osgp.adapter.ws.schema.microgrids.adhocmanagement.GetDataResponse.class)
+                .field("measurementResultSystemIdentifiers", "system").byDefault().register();
+
+        mapperFactory.getConverterFactory().registerConverter(new XMLGregorianCalendarToDateTimeConverter());
+    }
 }
