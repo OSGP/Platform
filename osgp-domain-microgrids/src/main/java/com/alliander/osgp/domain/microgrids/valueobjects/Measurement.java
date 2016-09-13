37ff3c025f9d315e68c9961f80bf6741c67b01bd
Joep-Simons <joep.simons@cgi.com>
2016-09-13 16:09:47 +0200

    Adds copyright headers

--- osgp-domain-microgrids/src/main/java/com/alliander/osgp/domain/microgrids/valueobjects/Measurement.java
+++ osgp-domain-microgrids/src/main/java/com/alliander/osgp/domain/microgrids/valueobjects/Measurement.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright 2014-2016 Smart Society Services B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
 package com.alliander.osgp.domain.microgrids.valueobjects;
 
 import java.io.Serializable;
@@ -10,9 +17,9 @@ public class Measurement extends MeasurementIdentifier implements Serializable {
      */
     private static final long serialVersionUID = 3315719218127525093L;
 
-    private int qualifier;
-    private DateTime time;
-    private double value;
+    private final int qualifier;
+    private final DateTime time;
+    private final double value;
 
     public Measurement(final int id, final String node, final int qualifier, final DateTime time, final double value) {
         super(id, node);
--- osgp-domain-microgrids/src/main/java/com/alliander/osgp/domain/microgrids/valueobjects/SystemFilter.java
+++ osgp-domain-microgrids/src/main/java/com/alliander/osgp/domain/microgrids/valueobjects/SystemFilter.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright 2014-2016 Smart Society Services B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
 package com.alliander.osgp.domain.microgrids.valueobjects;
 
 import java.io.Serializable;
@@ -8,8 +15,8 @@ public class SystemFilter extends SystemIdentifier implements Serializable {
 
     private static final long serialVersionUID = 2069822566541617223L;
 
-    private List<MeasurementFilter> measurementFilters;
-    private boolean all;
+    private final List<MeasurementFilter> measurementFilters;
+    private final boolean all;
 
     public SystemFilter(final int id, final String systemType, final List<MeasurementFilter> measurementFilters,
             final boolean all) {
