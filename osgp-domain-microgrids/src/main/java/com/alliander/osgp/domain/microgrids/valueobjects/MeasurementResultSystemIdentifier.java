2d2d7983996bb0273912b1a703d610daaf167a65
Joep-Simons <joep.simons@cgi.com>
2016-09-13 16:09:47 +0200

    Adds copyright headers

--- osgp-domain-microgrids/src/main/java/com/alliander/osgp/domain/microgrids/valueobjects/MeasurementResultSystemIdentifier.java
+++ osgp-domain-microgrids/src/main/java/com/alliander/osgp/domain/microgrids/valueobjects/MeasurementResultSystemIdentifier.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright 2014-2016 Smart Society Services B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
 package com.alliander.osgp.domain.microgrids.valueobjects;
 
 import java.util.ArrayList;
@@ -7,7 +14,7 @@ public class MeasurementResultSystemIdentifier extends SystemIdentifier {
 
     private static final long serialVersionUID = -6201476739756810987L;
 
-    private List<Measurement> measurements;
+    private final List<Measurement> measurements;
 
     public MeasurementResultSystemIdentifier(final int id, final String systemType,
             final List<Measurement> measurements) {
