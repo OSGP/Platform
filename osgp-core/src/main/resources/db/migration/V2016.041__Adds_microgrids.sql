15da93783f50eac7eb1d195358de47efccc2f575
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2016-09-16 12:58:09 +0200

    ZS-57 ~ Removes unneeded protocol info

--- osgp-core/src/main/resources/db/migration/V2016.041__Adds_microgrids.sql
+++ osgp-core/src/main/resources/db/migration/V2016.041__Adds_microgrids.sql
@@ -28,23 +28,3 @@ VALUES (current_date, current_date, 0, 'MICROGRIDS', '1.0',
  'domain-microgrids.1_0.osgp-core.1_0.requests',
  'osgp-core.1_0.domain-microgrids.1_0.responses'
 );
-
--- Adds the incoming and outgoing queues for the microgrids protocol queues
-INSERT INTO protocol_info(creation_time
-			 ,modification_time
-			 ,version
-			 ,protocol
-			 ,protocol_version
-			 ,outgoing_protocol_requests_queue
-			 ,incoming_protocol_responses_queue
-			 ,incoming_protocol_requests_queue
-			 ,outgoing_protocol_responses_queue)
-VALUES (current_date
-			 ,current_date
-			 ,0
-			 ,'IEC61850-RTU'
-			 ,'1.0'
-			 ,'protocol-iec61850-rtu.1_0.osgp-core.1_0.requests'
-			 ,'osgp-core.1_0.protocol-iec61850-rtu.1_0.responses'
-			 ,'osgp-core.1_0.protocol-iec61850-rtu.1_0.requests'
-			 ,'protocol-iec61850-rtu.1_0.osgp-core.1_0.responses');
