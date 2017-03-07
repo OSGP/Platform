2c20c2b71b62f80f4613c3341bba2f606ce849e9
Kevin Smeets <kevin.smeets@cgi.com>
2017-03-07 23:15:29 +0200

    OC-307 ~ Resolves critical false positive SonarQube issues.

--- osgp-adapter-ws-microgrids/src/main/java/com/alliander/osgp/adapter/ws/microgrids/application/config/PersistenceConfigCore.java
+++ osgp-adapter-ws-microgrids/src/main/java/com/alliander/osgp/adapter/ws/microgrids/application/config/PersistenceConfigCore.java
@@ -22,12 +22,12 @@ import com.alliander.osgp.domain.core.exceptions.PlatformException;
         com.alliander.osgp.domain.microgrids.repositories.RtuDeviceRepository.class })
 @Configuration
 @PropertySources({ @PropertySource("classpath:osgp-adapter-ws-microgrids.properties"),
-        @PropertySource(value = "file:${osgp/Global/config}", ignoreResourceNotFound = true),
-        @PropertySource(value = "file:${osgp/AdapterWsMicrogrids/config}", ignoreResourceNotFound = true), })
+    @PropertySource(value = "file:${osgp/Global/config}", ignoreResourceNotFound = true),
+    @PropertySource(value = "file:${osgp/AdapterWsMicrogrids/config}", ignoreResourceNotFound = true), })
 public class PersistenceConfigCore extends AbstractPersistenceConfigBase {
 
-    private static final String PROPERTY_NAME_DATABASE_PASSWORD = "db.password.core";
     private static final String PROPERTY_NAME_DATABASE_USERNAME = "db.username.core";
+    private static final String PROPERTY_NAME_DATABASE_PW = "db.password.core";
 
     private static final String PROPERTY_NAME_DATABASE_HOST = "db.host.core";
     private static final String PROPERTY_NAME_DATABASE_PORT = "db.port.core";
@@ -36,7 +36,7 @@ public class PersistenceConfigCore extends AbstractPersistenceConfigBase {
     private static final String PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN = "entitymanager.packages.to.scan.core";
 
     public PersistenceConfigCore() {
-        super("OSGP_CORE_MICROGRIDS", PROPERTY_NAME_DATABASE_USERNAME, PROPERTY_NAME_DATABASE_PASSWORD,
+        super("OSGP_CORE_MICROGRIDS", PROPERTY_NAME_DATABASE_USERNAME, PROPERTY_NAME_DATABASE_PW,
                 PROPERTY_NAME_DATABASE_HOST, PROPERTY_NAME_DATABASE_PORT, PROPERTY_NAME_DATABASE_NAME,
                 PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN, PersistenceConfigCore.class);
     }
