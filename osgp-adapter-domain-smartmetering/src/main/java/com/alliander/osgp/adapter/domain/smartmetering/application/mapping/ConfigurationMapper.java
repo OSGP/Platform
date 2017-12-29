521e5cf6c98df01ac3f1f5c5e08f5afaa0b145da
Bart van der Zwet <bart.van.der.zwet@cgi.com>
2017-12-29 10:32:27 +0000

    SLIM-1534 Reverts changes to push setup DTOs
    Mistakenly thought there were too many constructor arguments. The push
    setup DTOs fall just within the SonarQube boundaries though.
    This reverts the updates to prevent the need for custom converters in
    the Platform.

--- osgp-adapter-domain-smartmetering/src/main/java/com/alliander/osgp/adapter/domain/smartmetering/application/mapping/ConfigurationMapper.java
+++ osgp-adapter-domain-smartmetering/src/main/java/com/alliander/osgp/adapter/domain/smartmetering/application/mapping/ConfigurationMapper.java
@@ -13,8 +13,6 @@ import com.alliander.osgp.adapter.domain.smartmetering.application.mapping.custo
 import com.alliander.osgp.adapter.domain.smartmetering.application.mapping.customconverters.CosemDateTimeConverter;
 import com.alliander.osgp.adapter.domain.smartmetering.application.mapping.customconverters.FirmwareVersionConverter;
 import com.alliander.osgp.adapter.domain.smartmetering.application.mapping.customconverters.GetAllAttributeValuesResponseConverter;
-import com.alliander.osgp.adapter.domain.smartmetering.application.mapping.customconverters.PushSetupAlarmConverter;
-import com.alliander.osgp.adapter.domain.smartmetering.application.mapping.customconverters.PushSetupSmsConverter;
 
 import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.impl.ConfigurableMapper;
@@ -31,7 +29,5 @@ public class ConfigurationMapper extends ConfigurableMapper {
         mapperFactory.getConverterFactory().registerConverter(new AdministrativeStatusResponseConverter());
         mapperFactory.getConverterFactory().registerConverter(new FirmwareVersionConverter());
         mapperFactory.getConverterFactory().registerConverter(new GetAllAttributeValuesResponseConverter());
-        mapperFactory.getConverterFactory().registerConverter(new PushSetupAlarmConverter(this));
-        mapperFactory.getConverterFactory().registerConverter(new PushSetupSmsConverter(this));
     }
 }
