05a33e9f0b7991eb0f6d7e91be7bd3d638078cec
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2018-04-18 11:25:01 +0200

    FLEX-3816 ~ Adds astro offset fields to value object

--- osgp-domain-core/src/main/java/com/alliander/osgp/domain/core/valueobjects/ScheduleMessageDataContainer.java
+++ osgp-domain-core/src/main/java/com/alliander/osgp/domain/core/valueobjects/ScheduleMessageDataContainer.java
@@ -17,10 +17,28 @@ public class ScheduleMessageDataContainer implements Serializable {
      */
     private static final long serialVersionUID = 280691205768966372L;
 
-    private List<Schedule> scheduleList;
+    private final Short astronomicalSunriseOffset;
+    private final Short astronomicalSunsetOffset;
+
+    private final List<Schedule> scheduleList;
 
     public ScheduleMessageDataContainer(final List<Schedule> scheduleList) {
+        this(scheduleList, null, null);
+    }
+
+    public ScheduleMessageDataContainer(final List<Schedule> scheduleList, final Short astronomicalSunriseOffset,
+            final Short astronomicalSunsetOffset) {
+        this.astronomicalSunriseOffset = astronomicalSunriseOffset;
+        this.astronomicalSunsetOffset = astronomicalSunsetOffset;
         this.scheduleList = scheduleList;
+    }
+
+    public Short getAstronomicalSunriseOffset() {
+        return this.astronomicalSunriseOffset;
+    }
+
+    public Short getAstronomicalSunsetOffset() {
+        return this.astronomicalSunsetOffset;
     }
 
     public List<Schedule> getScheduleList() {
